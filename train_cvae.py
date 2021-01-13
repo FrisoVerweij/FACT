60e7a68e535111e774a41fd19d6aabef32acf9a7
SimonMariani <simon.mariani65@gmail.com>
2021-01-13 15:16:10 +0100

    added algorithm 1 and one extra boolean parameter find_params to the config

--- train_cvae.py
+++ train_cvae.py
@@ -2,6 +2,7 @@ import torch
 import yaml
 from utils import *
 import time
+import sys
 
 def train_cvae(encoder, decoder, classifier, dataloader, n_epochs, optimizer, device, params, use_causal_effect, lam_ML):
     # --- train ---
@@ -62,6 +63,7 @@ def joint_uncond(params, decoder, classifier, device):
             beta = np.random.randn(params['n_beta'])
             zs[j, :params['n_alpha']] = alpha
             zs[j, params['n_alpha']:] = beta
+
         # decode and classify batch of Nbeta samples with same alpha
         xhat = decoder(torch.from_numpy(zs).float().to(device))
         with torch.no_grad():
@@ -76,6 +78,139 @@ def joint_uncond(params, decoder, classifier, device):
     return negCausalEffect, info
 
 
+def algorithm1(classifier, train_data, test_data, epochs, device, params, use_causal, config):
+    # step 1
+    plateau = False
+    K, L, lamb = 0, 0, 0.0  # K = alpha L = beta
+    nll_best = 10000.0  # initialize the value to a very high value
+    start_time = time.time()
+    optimize_on = 'nll'
+    while (not plateau):
+        L += 1
+        _, _, average_nll, _, _ = \
+            train_and_test(optimize_on, K,L,lamb, classifier, train_data, test_data, epochs, device, params, use_causal, config)
+
+        if average_nll < nll_best:
+            nll_best = average_nll
+        else:
+            plateau = True
+
+        print("step 1, finished iteration, [best K: %d] [best L: %d] [best lamb: %f] [average nll: %f] time: %4.4f" %
+              (K,L,lamb, average_nll, time.time() - start_time))
+
+    print("step 1, finished, [best K: %d] [best L: %d] [best lamb: %f] [best loss: %f] time: %4.4f" %
+          (K, L, lamb, nll_best, time.time() - start_time))
+
+    # step 2 and 3
+    plateau = False
+    causal_best = 10000.0
+    start_time = time.time()
+    optimize_on = 'both'
+    while(not plateau):
+        K += 1
+        L -= 1
+        lamb = 0.0 # not sure if lambda resets after every iteration
+        approaches = False
+
+        # step 3
+        while(not approaches):
+            lamb += 0.01
+            _, _, average_nll, average_causal, _ = \
+                train_and_test(optimize_on, K, L, lamb, classifier, train_data, test_data, epochs, device, params, use_causal, config)
+
+            # note that here lambda is not incremented until the best one is found but until it reaches the desired
+            # accuracy
+            if average_nll < nll_best:
+                approaches = True
+
+            print("step 2, finished iteration, [best K: %d] [best L: %d] [best lamb: %f] [average nll: %f] time: %4.4f" %
+                  (K, L, lamb, average_nll, time.time() - start_time))
+
+        if average_causal < causal_best:
+            causal_best = average_causal
+        else:
+            plateau = True
+
+        print("step 3, finished iteration, [best K: %d] [best L: %d] [best lamb: %f] [average causal: %f] time: %4.4f" %
+              (K, L, lamb, average_causal, time.time() - start_time))
+
+    print("finished algorithm, [best K: %d] [best L: %d] [best lamb: %f] [best nll: %f] [best causal: %f] time: %4.4f" %
+          (K, L, lamb, nll_best, causal_best, time.time() - start_time))
+
+def train_and_test(optimize_on, K, L, lamb, classifier, train_data, test_data, epochs, device, params, use_causal, config):
+    z_dim = K + L
+
+    encoder, decoder = select_vae_model(config, z_dim)
+    encoder, decoder = encoder.to(device), decoder.to(device)
+    optimizer = select_optimizer(config, encoder, decoder)
+
+    params['z_dim'], params['n_alpha'], params['n_beta'] = z_dim, K, L
+
+    encoder_trained, decoder_trained = train(optimize_on, encoder, decoder,optimizer, classifier, train_data, epochs,
+                                        device, params, use_causal, lamb)
+
+    average_nll, average_causal_effect, average_total = test(encoder_trained, decoder_trained, classifier, test_data,
+                                        device, params, use_causal, lamb)
+
+    return encoder_trained, decoder_trained, average_nll, average_causal_effect, average_total
+
+def train(optimize_on, encoder, decoder, optimizer, classifier, train_dataset, epochs, device, params, use_causal, lamb):
+
+    for epoch in range(epochs):  # n_epochs determines how long we will train the model before we finish
+        for imgs, targets in train_dataset:
+            imgs, targets = imgs.to(device), targets.to(device)
+            optimizer.zero_grad()
+
+            latent_out, mu, logvar = encoder(imgs)
+            x_generated = decoder(latent_out)
+
+            nll, nll_mse, nll_kld = VAE_LL_loss(imgs, x_generated, logvar, mu)
+
+            if optimize_on == 'nll':
+                loss = nll
+
+            elif optimize_on == 'causal':
+                causalEffect, ceDebug = joint_uncond(params, decoder, classifier, device)
+                loss = causalEffect
+
+            elif optimize_on == 'both':
+                causalEffect, ceDebug = joint_uncond(params, decoder, classifier, device)
+                loss = use_causal * causalEffect + lamb * nll
+
+            loss.backward()
+            optimizer.step()
+
+    return encoder, decoder
+
+def test(encoder, decoder,classifier, test_data, device, params, use_causal, lamb):
+    # Test the model based on its average test loss
+    encoder.eval(); decoder.eval()
+    total_nll = 0; total_causal_effect = 0; total_loss = 0
+    batch = 0
+    with torch.no_grad():
+        for imgs, targets in test_data:
+            imgs, targets = imgs.to(device), targets.to(device)
+
+            latent_out, mu, logvar = encoder(imgs)
+            x_generated = decoder(latent_out)
+
+            nll, nll_mse, nll_kld = VAE_LL_loss(imgs, x_generated, logvar, mu)
+            causalEffect, ceDebug = joint_uncond(params, decoder, classifier, device)
+            loss = use_causal * causalEffect + lamb * nll
+
+            total_nll += nll
+            total_causal_effect += causalEffect
+            total_loss += loss
+
+            batch += 1
+
+        average_nll = total_nll / (batch + 1)
+        average_causal_effect = total_causal_effect / (batch + 1)
+        average_total = total_loss / (batch + 1)
+
+    return average_nll, average_causal_effect, average_total
+
+
 if __name__ == "__main__":
     # Set seeds to make test reproducible
     torch.manual_seed(0)
@@ -84,7 +219,8 @@ if __name__ == "__main__":
     # Parse the arguments
     parser = argparse.ArgumentParser()
     #parser.add_argument('--config', default='config/mnist_3_8.yml')
-    parser.add_argument('--config', default='config/cifar10_sasha.yml')
+    #parser.add_argument('--config', default='config/cifar10_sasha.yml')
+    parser.add_argument('--config', default='config/mnist_3_8_algorithm.yml')
 
     args = parser.parse_args()
     config = yaml.load(open(args.config, "r"))
@@ -93,9 +229,8 @@ if __name__ == "__main__":
     # The device to run the model on
     device = config['device']
 
-    # The latent variable dimensions z_dim and the number of pixels x_dim
+    # The latent variable dimensions z_dim
     z_dim = config['n_alpha'] + config['n_beta']
-    x_dim = config['image_size']**2
 
     # The decoder and encoder to use
     encoder, decoder = select_vae_model(config, z_dim)
@@ -126,7 +261,13 @@ if __name__ == "__main__":
         "n_beta": config['n_beta']
     }
 
-    encoder, decoder = train_cvae(encoder, decoder, classifier, train_dataset, config['epochs'], optimizer,
-                                 device, params, config['use_causal'], config['lam_ml'])
+    print(config)
+    if not config['find_params']:
+        encoder, decoder = train_cvae(encoder, decoder, classifier, train_dataset, config['epochs'], optimizer,
+                                     device, params, config['use_causal'], config['lam_ml'])
+    else:
+        algorithm1(classifier, train_dataset, test_dataset, config['epochs'], device, params, config['use_causal'], config)
+
+
 
 
