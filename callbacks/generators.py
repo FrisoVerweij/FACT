f3e6933b39f18a0a69b2bd0e913a992238feb2a0
Gerson <gerson.foks@gmail.com>
2021-01-21 10:14:12 +0100

    added ipynb fixes #65

--- callbacks/generators.py
+++ callbacks/generators.py
@@ -106,23 +106,15 @@ class GenerateCallbackLatent(pl.Callback):
             pl_module - The VAE model that is currently being trained.
             epoch - The epoch number to use for TensorBoard logging and saving of the files.
         """
-        results = []
-        for i in range(self.n_samples):
-            samples, y = pl_module.sample(self.to_sample_from[i].unsqueeze(0))
-            samples = add_border_to_samples(samples, y, border_size=self.border_size, to_rgb=self.to_rgb)
-            results.append(samples)
 
+        results = create_samples(self.to_sample_from, pl_module, border_size=self.border_size, to_rgb=self.to_rgb, )
+        grids = create_latent_grids(results, self.latent_dimensions)
         ### Loop over the latent dimensions
-        for i in range(self.latent_dimensions):
+        for i, grid in enumerate(grids):
 
             latent_dim_samples = []
             start_index = 7 * i
             end_index = start_index + 7
-
-            for samples in results:
-                latent_dim_samples += samples[start_index: end_index]
-
-            grid = make_grid(latent_dim_samples, nrow=7)
             name = 'latent_samples_{}_{}'.format(i, epoch)
             logger = trainer.logger.experiment
             logger.add_image('latent_sample_{}'.format(i), grid, epoch)
@@ -189,3 +181,29 @@ def combine_border_and_sample(sample, border, border_size=5):
     result += border
     result[:, border_size:-border_size, border_size: -border_size] = sample
     return result
+
+
+def create_samples(to_sample_from, model, to_rgb=True, border_size=5, ):
+    results = []
+    for i in range(len(to_sample_from)):
+        samples, y = model.sample(to_sample_from[i].unsqueeze(0))
+        samples = add_border_to_samples(samples, y, to_rgb=to_rgb, border_size=border_size, )
+        results.append(samples)
+    return results
+
+
+def create_latent_grids(results, latent_dimensions, ):
+    grids = []
+    ### Loop over the latent dimensions
+    for i in range(latent_dimensions):
+
+        latent_dim_samples = []
+        start_index = 7 * i
+        end_index = start_index + 7
+
+        for samples in results:
+            latent_dim_samples += samples[start_index: end_index]
+
+        grid = make_grid(latent_dim_samples, nrow=7)
+        grids.append(grid)
+    return grids
--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ import torch
 from models import models_classifiers, models_vae, CNN_classifier_author
 import numpy as np
 
+
 def select_dataloader(config):
     '''
     Selects a dataloader given the hyperparameters
@@ -19,7 +20,8 @@ def select_dataloader(config):
 
     if config["dataset"] == "mnist":
         if config["classifier"] == "mnist_dummy":
-            return dataset.get_mnist_dataloaders(batch_size=config["batch_size"], digits_to_include=mnist_digits, dummy=True)
+            return dataset.get_mnist_dataloaders(batch_size=config["batch_size"], digits_to_include=mnist_digits,
+                                                 dummy=True)
         else:
             return dataset.get_mnist_dataloaders(batch_size=config["batch_size"], digits_to_include=mnist_digits)
 
@@ -34,6 +36,7 @@ def select_dataloader(config):
 
     else:
         raise Exception("No valid dataset selected!")
+
 
 def select_classifier(config):
     '''
@@ -51,7 +54,6 @@ def select_classifier(config):
         if config["model_name"][-3:] == ".pt":
             print("load .pt")
 
-
             return CNN_classifier_author.CNN(output_dim)
         else:
             return models_classifiers.MNIST_CNN(output_dim)
@@ -65,6 +67,7 @@ def select_classifier(config):
         return models_classifiers.BiggestDummy()
     else:
         raise Exception("No valid model selected!")
+
 
 def select_optimizer(config, model, model_2=None):
     '''
@@ -81,9 +84,11 @@ def select_optimizer(config, model, model_2=None):
     if config["optimizer"] == "SGD":
         return torch.optim.SGD(params, lr=config["lr"], momentum=config["momentum"])
     elif config["optimizer"] == "Adam":
-        return torch.optim.Adam(params, lr=config["lr"], betas=(config["b1"], config["b2"]), weight_decay=config["weight_decay"])
+        return torch.optim.Adam(params, lr=config["lr"], betas=(config["b1"], config["b2"]),
+                                weight_decay=config["weight_decay"])
     else:
         raise Exception("No valid optimizer selected!")
+
 
 def select_vae_model(config):
     if config["vae_model"] in ["mnist_cvae", "fmnist_cvae"]:
@@ -93,15 +98,19 @@ def select_vae_model(config):
         encoder = models_vae.Encoder_cifar10(config['z_dim'], 3, config["image_size"] ** 2)
         decoder = models_vae.Decoder_cifar10(config['z_dim'], 3, config["image_size"] ** 2)
     elif config["vae_model"] == "cifar10_cvae_sasha":
-        encoder = models_vae.Encoder_cifar10_sasha(config['z_dim'], 3, config["image_size"])  # note that here we do not square the image size
+        encoder = models_vae.Encoder_cifar10_sasha(config['z_dim'], 3, config[
+            "image_size"])  # note that here we do not square the image size
         decoder = models_vae.Decoder_cifar10_sasha(config['z_dim'], 3, config["image_size"])
     elif config["vae_model"] == "cifar10_cvae_captain":
-        encoder = models_vae.Encoder_captain(config['z_dim'], 3, config["image_size"], config["image_size"], config["device"])
-        decoder = models_vae.Decoder_captain(config['z_dim'], 3, config["image_size"], config["image_size"], config["device"])
+        encoder = models_vae.Encoder_captain(config['z_dim'], 3, config["image_size"], config["image_size"],
+                                             config["device"])
+        decoder = models_vae.Decoder_captain(config['z_dim'], 3, config["image_size"], config["image_size"],
+                                             config["device"])
     else:
         raise Exception("No valid encoder/decoder selected!")
 
     return encoder, decoder
+
 
 def weights_init_normal(m):
     classname = m.__class__.__name__
@@ -110,6 +119,7 @@ def weights_init_normal(m):
     elif classname.find('linear') != -1:
         torch.nn.init.normal_(m.weight.data, 0.0, 0.02)
         torch.nn.init.constant_(m.bias.data, 0.0)
+
 
 def prepare_variables_pl(config):
     # The device to run the model on
@@ -131,9 +141,13 @@ def prepare_variables_pl(config):
         "channel_dimension": 1,
         "x_dim": x_dim
     }
+    base_path = config['save_dir'] + config["model_name"]
+    config['encoder_path'] = base_path + '_encoder'
+    config['decoder_path'] = base_path + '_decoder'
 
     config = {**config, **params}
     return config
+
 
 def VAE_LL_loss(Xbatch, Xest, logvar, mu):
     batch_size = Xbatch.shape[0]
@@ -142,6 +156,7 @@ def VAE_LL_loss(Xbatch, Xest, logvar, mu):
     mse = 1. / batch_size * sse_loss(Xest, Xbatch)
     auto_loss = mse + KLD
     return auto_loss, mse, KLD
+
 
 def joint_uncond(params, decoder, classifier, device):
     eps = 1e-8
@@ -168,18 +183,74 @@ def joint_uncond(params, decoder, classifier, device):
     info = {"xhat": xhat, "yhat": yhat}
     return negCausalEffect, info
 
+
 def reconstruction_loss(x_reconstructed, x):
     return nn.BCELoss(reduction='sum')(x_reconstructed, x) / x.size(0)
 
+
 def kl_divergence_loss(mean, logvar):
-    return ((mean**2 + logvar.exp() - 1 - logvar) / 2).mean()
+    return ((mean ** 2 + logvar.exp() - 1 - logvar) / 2).mean()
 
 
 def to_classifier_config(config):
-    return  {**config['classifier'], **config['general'],**config['dataset'], }
+    return {**config['classifier'], **config['general'], **config['dataset'], }
+
 
 def to_vae_config(config):
-    return {**config['vae'], **config['general'],**config['dataset'], "classifier": config["classifier"]["classifier"]}
+    return {**config['vae'], **config['general'], **config['dataset'], "classifier": config["classifier"]["classifier"]}
+
 
 def to_visualize_config(config):
     return {**config['vae'], **config['general'], **config['dataset'], "classifier": config["classifier"]["classifier"]}
+
+
+def load_classifier(config):
+    # Select and load the classifier
+    classifier = select_classifier(config)
+    if config["classifier"] != "mnist_dummy":
+
+        if config["model_name"][-3:] == ".pt":
+            classifier.load_state_dict(
+                torch.load(config['save_dir'] + config['model_name'])['model_state_dict_classifier'])
+        else:
+            classifier.load_state_dict(
+                torch.load(config['save_dir'] + config['classifier'] + "_" + config['model_name']))
+    return classifier
+
+
+def get_x_vals(val_loader, n_classes=2, n_for_each_class=4):
+    ###
+
+    # Here we get a sufficient number of samples to get the images from
+    data, targets = next(iter(val_loader))
+    for x, y in val_loader:
+        data = torch.cat([data, x], dim=0)
+        targets = torch.cat([targets, y], dim=0)
+
+        # Here we make sure that we have all the classes and that we have enough samples
+        if len(torch.unique(targets)) >= n_classes:
+            y_val = targets.numpy()
+            indices = []
+            for i in range(n_classes):
+                indices += list(np.where(y_val == i)[0])[:n_for_each_class]
+
+            if indices == n_classes * n_for_each_class:
+                break
+
+    indices = torch.tensor(indices)
+    x_val = torch.index_select(data, 0, indices)
+
+    return x_val
+
+
+def save_model(model, path):
+    torch.save(model.state_dict(), path)
+
+
+def load_encoder_decoder(config):
+    # Select the encoder, decoder and optimizer
+    encoder, decoder = select_vae_model(config)
+
+    encoder.load_state_dict(torch.load(config['encoder_path']))
+    decoder.load_state_dict(torch.load(config['decoder_path']))
+    return encoder, decoder
